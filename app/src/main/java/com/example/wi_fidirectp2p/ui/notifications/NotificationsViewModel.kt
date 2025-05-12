64f9be0530a22dbc6a7fa4178e4ac75ea961fe73
Orwa Houry <Orwa.Houry@Student.HTW-Berlin.de>
2025-05-12 18:21:34 +0200

    First commit

---
+++ app/src/main/java/com/example/wi_fidirectp2p/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.wi_fidirectp2p.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
